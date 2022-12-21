f36485807202404900ee6973b106e00556fa6c21
HugoAissa <112508131+HugoAissa@users.noreply.github.com>
2022-12-21 20:58:33 -0300

    Add files via upload

---
+++ personalizado.js
@@ -0,0 +1,92 @@
+function Daily() {
+    //WORK
+    document.getElementById("titulo").innerHTML = "Work"
+    document.getElementById("hrs").innerHTML = "5hrs"
+    document.getElementById("last").innerHTML = "Last Week = 7hrs"
+    //FIM WORK
+
+    //EXERCISE
+    document.getElementById("titulo2").innerHTML = "Exercise"
+    document.getElementById("hrs2").innerHTML = "1hrs"
+    document.getElementById("last2").innerHTML = "Last Week = 1hrs"
+    //FIM EXERCISE
+
+    //SOCIAL
+    document.getElementById("titulo3").innerHTML = "Social"
+    document.getElementById("hrs3").innerHTML = "1hrs"
+    document.getElementById("last3").innerHTML = "Last Week = 3hrs"
+    //FIM SOCIAL
+
+    
+    //PLAY
+    document.getElementById("titulo4").innerHTML = "Play"
+    document.getElementById("hrs4").innerHTML = "1hrs"
+    document.getElementById("last4").innerHTML = "Last Week = 2hrs"
+    //FIM PLAY
+
+
+}
+
+
+function Weekly() {
+    //WORK
+    document.getElementById("titulo").innerHTML = "Work"
+    document.getElementById("hrs").innerHTML = "32hrs"
+    document.getElementById("last").innerHTML = "Last Week = 36hrs"
+    //FIM WORK
+
+    //EXERCISE
+    document.getElementById("titulo2").innerHTML = "Exercise"
+    document.getElementById("hrs2").innerHTML = "4hrs"
+    document.getElementById("last2").innerHTML = "Last Week = 5hrs"
+    //FIM EXERCISE
+
+    //SOCIAL
+    document.getElementById("titulo3").innerHTML = "Social"
+    document.getElementById("hrs3").innerHTML = "5hrs"
+    document.getElementById("last3").innerHTML = "Last Week = 10hrs"
+    //FIM SOCIAL
+
+    
+    //PLAY
+    document.getElementById("titulo4").innerHTML = "Play"
+    document.getElementById("hrs4").innerHTML = "10hrs"
+    document.getElementById("last4").innerHTML = "Last Week = 8hrs"
+    //FIM PLAY
+
+
+}
+
+
+function Monthly(){
+        //WORK
+        document.getElementById("titulo").innerHTML = "Work"
+        document.getElementById("hrs").innerHTML = "103hrs"
+        document.getElementById("last").innerHTML = "Last Week = 128hrs"
+        //FIM WORK
+    
+        //EXERCISE
+        document.getElementById("titulo2").innerHTML = "Exercise"
+        document.getElementById("hrs2").innerHTML = "23hrs"
+        document.getElementById("last2").innerHTML = "Last Week = 29hrs"
+        //FIM EXERCISE
+    
+        //SOCIAL
+        document.getElementById("titulo3").innerHTML = "Social"
+        document.getElementById("hrs3").innerHTML = "21hrs"
+        document.getElementById("last3").innerHTML = "Last Week = 23hrs"
+        //FIM SOCIAL
+    
+        
+        //PLAY
+        document.getElementById("titulo4").innerHTML = "Play"
+        document.getElementById("hrs4").innerHTML = "23hrs"
+        document.getElementById("last4").innerHTML = "Last Week = 29hrs"
+        //FIM PLAY
+    
+}
+
+
+
+
+
